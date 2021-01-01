1b2794f6ea3e1b2d5c61a6d73786d30f18ca0f2b
ReshamSaharan11 <43739491+ReshamSaharan11@users.noreply.github.com>
2021-01-01 20:37:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input});
-      fetch('http://localhost:3000/imageurl', {
+      fetch('https://infinite-sierra-90608.herokuapp.com/imageurl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
