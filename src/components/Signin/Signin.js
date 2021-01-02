3ea8a5e122d5c019bcc8f462b2455cd84cd66bd5
ReshamSaharan11 <43739491+ReshamSaharan11@users.noreply.github.com>
2021-01-02 20:50:29 +0530

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://infinite-sierra-90608.herokuapp.com/signin', {
+    fetch('https://warm-eath-96837.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
